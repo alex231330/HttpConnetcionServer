75bd1e009eb0ea3272444c786584b62968e2c18a
Alexandr <alexklatch@gmail.com>
2017-06-26 00:03:02 +0300

    Final version of server

---
+++ src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.*;
+import java.net.*;
+
+import com.company.ServerHandler;
+
+public class Main {
+    static Socket s;
+
+    public static void main(String[] args) throws Throwable {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                InputStream is = null;
+                OutputStream os = null;
+                try {
+                    is = s.getInputStream();
+                    os = s.getOutputStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                while (true) {
+                    try {
+                        String s = br.readLine().toString();
+                        if (s != null) {
+                            break;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                String res = "Resp";
+                // http://localhost:8080/
+                try {
+                    os.write(res.getBytes());
+                    os.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        ServerSocket server = new ServerSocket(8080);
+        String stroke = System.in.toString();
+        s = server.accept();
+        System.out.println("Client connected");
+        thread.start();
+
+    }
+}
---
+++ src/com/company/ServerHandler.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by alex231330 on 22.06.17.
+ */
+public class ServerHandler {
+}
